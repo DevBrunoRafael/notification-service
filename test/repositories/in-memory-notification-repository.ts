86e283d72d2e353436ca7b856d985961ab849bff
Bruno Rafael <brunorafaeldls2016@gmail.com>
2022-12-18 23:13:07 -0300

    add inmemory database

--- test/repositories/in-memory-notification-repository.ts
+++ test/repositories/in-memory-notification-repository.ts
@@ -7,4 +7,36 @@ export class InMemoryNotificationRpository implements NotificationRepository {
   async create(notification: Notification) {
     this.notifications.push(notification);
   }
+
+  async findById(notificationId: string): Promise<Notification | null> {
+    const notification = this.notifications.find(
+      (notification) => notification.id === notificationId,
+    );
+
+    if (!notification) return null;
+
+    return notification;
+  }
+
+  async save(notification: Notification) {
+    const notificationIndex = this.notifications.findIndex(
+      (ntf) => ntf.id === notification.id,
+    );
+
+    if (notificationIndex >= 0) {
+      this.notifications[notificationIndex] = notification;
+    }
+  }
+
+  async countManyByRecipientId(recipientId: string): Promise<number> {
+    return this.notifications.filter(
+      (notification) => notification.recipientId === recipientId,
+    ).length;
+  }
+
+  async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+    return this.notifications.filter(
+      (Notification) => Notification.recipientId === recipientId,
+    );
+  }
 }
